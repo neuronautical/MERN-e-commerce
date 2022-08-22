1793e1c56af7b8a9363a9dd8f6ccc0cfdd425ffb
neuronautical <llcodingprojects@gmail.com>
2022-08-22 12:33:47 -0700

    Functionality Fixes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,38 +1,32 @@
-import data from "./data";
-
+import {BrowserRouter, Link, Route, Routes} from 'react-router-dom'
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 
 function App() {
   return (
+  
+    <BrowserRouter>
+    
+
     <div>
       <header>
-       <a href="/">amazona</a>
+       <Link to="/">amazona</Link>
       </header>
       <main>
-        <h1>Featured Products</h1>
-        <div className="products">
-        { data.products.map(product => (
-            <div className="product" key={product.slug}>
-              
-              <a href={`/product/${product.slug}`}>
-                <img src={product.image} alt={product.name}/>
-              </a>
-
-              
-              <div class="product-info">
-              <a href={`/product/${product.slug}`}>
-                <p> {product.name} </p>
-              </a>
-
-                <p> <strong>${product.price}</strong> </p>
-                <button> Add to cart</button>
-              </div>
-              
-          </div>
-        ))}
-        </div>
-
+        <Routes>
+          <Route path ="/" element={<HomeScreen/>}/> 
+          <Route path ="/product/:slug" element={<ProductScreen />}/> 
+        </Routes>
+       
       </main>
-    </div>
+    </div>    
+    
+    
+    </BrowserRouter>
+
+
+
+
   );
 }
 
